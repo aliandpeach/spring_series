2ba8a213405ed995e536a8f7ce8729b6ce6c0444
aliandpeach <aliandpeach@gmail.com>
2021-03-15 09:33:52 +0800

    commit

--- spring_annotation_website/src/main/java/com/yk/base/config/RootConfig.java
+++ spring_annotation_website/src/main/java/com/yk/base/config/RootConfig.java
@@ -2,7 +2,9 @@ package com.yk.base.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -13,6 +15,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @ComponentScan(basePackages = {"com.yk"}, excludeFilters =
         {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)})
+
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableTransactionManagement
 public class RootConfig
 {
 
