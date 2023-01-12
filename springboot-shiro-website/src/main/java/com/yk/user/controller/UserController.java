bc0d6c1a6a2d108f2f967ac02dbe2dd13ca89040
aliandpeach <aliandpeach@gmail.com>
2023-01-12 11:03:28 +0800

    commit

--- springboot-shiro-website/src/main/java/com/yk/user/controller/UserController.java
+++ springboot-shiro-website/src/main/java/com/yk/user/controller/UserController.java
@@ -6,6 +6,8 @@ import com.yk.user.service.UserService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +19,8 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController
 {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
@@ -27,6 +31,7 @@ public class UserController
     public BaseResponse<List<User>> queryAllList()
     {
         Object principal = SecurityUtils.getSubject().getPrincipal();
+        logger.debug("all user info query, current user : {}", principal);
         return new BaseResponse<>(200, "", userService.queryAllList());
     }
 }
